bdf309b6151cfee3f882506948b0c48362775717
Aman Sinha <saritasinha4992@gmail.com>
2026-06-29 13:25:35 +0530

    fix: correct home.css import path

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./home.css";
+import "./home/home.css";
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
